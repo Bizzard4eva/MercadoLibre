dc3f410b74cb28a168188f7a62cb9054d7b29d25
Bizzard <jevbxx@gmail.com>
2025-06-30 06:09:48 -0500

    Include para la Barra de Busqueda

--- app/src/main/java/com/proyecto/mercadolibre/ui/ProductosFavoritosActivity.kt
+++ app/src/main/java/com/proyecto/mercadolibre/ui/ProductosFavoritosActivity.kt
@@ -1,16 +1,27 @@
 package com.proyecto.mercadolibre.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.proyecto.mercadolibre.R
+import com.proyecto.mercadolibre.adaptadores.ProductoAdaptador
+import com.proyecto.mercadolibre.database.AppDatabase
+import com.proyecto.mercadolibre.database.DatabaseProvider
 import com.proyecto.mercadolibre.databinding.ActivityProductosFavoritosBinding
+import com.proyecto.mercadolibre.entidades.Usuario
+import com.proyecto.mercadolibre.utilitarios.Sesion
+import com.proyecto.mercadolibre.utilitarios.Util
 
 class ProductosFavoritosActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProductosFavoritosBinding
+    private lateinit var db: AppDatabase
+    private var usuario: Usuario? = Sesion.usuarioActual
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +29,50 @@ class ProductosFavoritosActivity : AppCompatActivity() {
 
         binding = ActivityProductosFavoritosBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        db = DatabaseProvider.getDatabase(this@ProductosFavoritosActivity)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         //TODO
+
+        if (usuario == null) {
+            Util.showToast(this, "Debe iniciar sesión")
+            finish()
+            return
+        }
+        Thread {
+            val favoritos = db.favoritoDAO().listarFavoritosPorUsuario(usuario!!.idUsuario)
+
+            val productos = favoritos.mapNotNull {
+                db.productoDAO().getProducto(it.idProducto)
+            }
+
+            val urls = productos.map {
+                val lista = db.imagenProductoDAO().listarUrlsImagenesDeProducto(it.idProducto)
+                lista.firstOrNull() ?: ""
+            }
+
+            runOnUiThread {
+                binding.tvTituloPF.text = "Mis Productos Favoritos"
+
+                Glide.with(this)
+                    .load("https://http2.mlstatic.com/storage/splinter-admin/o:f_webp,q_auto:best/1747425559898-41324ba0-3290-11f0-940a-53fc307ffb6d.jpg")
+                    .placeholder(R.drawable.carrusel_uno)
+                    .into(binding.imgBannerPF)
+
+                val adapter = ProductoAdaptador(productos, urls) { producto ->
+                    val intent = Intent(this, ProductoActivity::class.java)
+                    intent.putExtra("idProducto", producto.idProducto)
+                    startActivity(intent)
+                }
+
+                binding.recyclerViewPF.adapter = adapter
+                binding.recyclerViewPF.layoutManager = LinearLayoutManager(this)
+            }
+        }.start()
 
     }
 }
