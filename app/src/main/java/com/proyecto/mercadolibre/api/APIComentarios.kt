1adbe7962068e1117aeaccc8a77d68270957d716
Abel Sulca Espinoza <abelsulcaespinoza@gmail.com>
2025-06-30 19:45:14 -0500

    comentarios

--- app/src/main/java/com/proyecto/mercadolibre/api/APIComentarios.kt
+++ app/src/main/java/com/proyecto/mercadolibre/api/APIComentarios.kt
@@ -8,13 +8,20 @@ import retrofit2.http.GET
 
 object APIComentarios {
 
+    // Crea una instancia de Retrofit con la URL base definida en las constantes,
+    // y agrega un convertidor para manejar respuestas JSON (usando Gson)
     private  val builder = Retrofit.Builder().baseUrl(Constantes.rutaws)
         .addConverterFactory(GsonConverterFactory.create())
 
+    // Define la interfaz del servicio web (endpoints disponibles)
     interface servicioWeb{
+        // Metodo que realiza una petición GET a "comments"
+        // y espera una lista de objetos Comentario como respuesta
         @GET("comments")
         fun getComentarios() : Call<List<Comentario>>
     }
+
+    // Metodo que construye e instancia el servicio web para poder usarlo
     fun build() : servicioWeb {
         return builder.build().create(servicioWeb::class.java)
     }
--- app/src/main/java/com/proyecto/mercadolibre/api/Constantes.kt
+++ app/src/main/java/com/proyecto/mercadolibre/api/Constantes.kt
@@ -3,7 +3,10 @@ package com.proyecto.mercadolibre.api
 class Constantes {
     companion object
     {
+        // URL base del servicio web (en este caso, una API de prueba pública)
         val rutaws = "https://jsonplaceholder.typicode.com/"
+
+        // Variable para almacenar la cantidad de comentarios obtenidos (se puede modificar)
         var cant_comentarios: Int = 0
     }
 }
